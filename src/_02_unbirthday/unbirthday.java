eb8a7aad446aa41309c11074bff713df316619af
ingraham.brenden <ingraham.brenden@SEMS-259-34.SEMS259.LAB>
2019-11-20 14:57:56 -0800

    yeet

---
+++ src/_02_unbirthday/unbirthday.java
@@ -0,0 +1,51 @@
+package _02_unbirthday;
+
+import javax.swing.JOptionPane;
+
+public class unbirthday {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+	String Unbirthday =	JOptionPane.showInputDialog("When is your birthday?");
+	
+	
+	if (Unbirthday. equals()))}
+	
+	
+	
+	
+		JOptionPane.showInputDialog("if its today, happy birthday if not happy unbirthday");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
