c5bf88e1c7a7e5606fd59d8b321f657e7c290276
ingraham.brenden <ingraham.brenden@SEMS-259-34.SEMS259.LAB>
2019-12-11 14:57:38 -0800

    12/11/19

--- src/_07_riddler/TheRiddler.java
+++ src/_07_riddler/TheRiddler.java
@@ -11,8 +11,8 @@ public class TheRiddler {
 		// 1. Make a variable to hold the score
 int score = 0;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
- JOptionPane.showInputDialog("The more you take, the more you leave behind. What am I?")
-
+  String mason = JOptionPane.showInputDialog("The more you take, the more you leave behind. What am I?");
+	
 
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
 
@@ -21,7 +21,7 @@ int score = 0;
 		// 6. Add some more riddles
 
 		// 2. Make a pop up to show the score.
+	
+	JOptionPane.showMessageDialog(null,"your score was" + score + "good job");
 	}
-	JOptionPane.showMessageDialog("your score was"+ score + "great job")
 }
-
